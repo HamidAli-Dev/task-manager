3d5615059a1781b36201dc5617a18a618ada7ecf
Hamid Ali <hamidali.webdev@gmail.com>
2026-01-07 10:33:47 +0500

    add initial frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl underline font-bold">
+          Full stack task manager
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
